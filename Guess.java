4fda6b16662e963d4bf6ed527aebc0dc18f72430
3ndryw <andreacostantini03@gmail.com>
2025-12-29 21:39:29 +0100

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -1,2 +1,30 @@
 public class Guess {
+    private SuspectC suspect;
+    private WeaponC weapon;
+    private RoomC room;
+
+    // GETTERS AND SETTERS
+    public SuspectC getSuspect() {
+        return suspect;
+    }
+
+    public void setSuspect(SuspectC suspect) {
+        this.suspect = suspect;
+    }
+
+    public WeaponC getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(WeaponC weapon) {
+        this.weapon = weapon;
+    }
+
+    public RoomC getRoom() {
+        return room;
+    }
+
+    public void setRoom(RoomC room) {
+        this.room = room;
+    }
 }
