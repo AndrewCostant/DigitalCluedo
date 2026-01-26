abf655fe010ce23424fa9647512ec9ceeb621543
3ndryw <andreacostantini03@gmail.com>
2026-01-26 12:06:15 +0100

    Update Cell.java

--- src/domain/Cell.java
+++ src/domain/Cell.java
@@ -16,4 +16,9 @@ public interface Cell {
 	 */
 	public String doAction(SuspectC suspect, WeaponC weapon, Player player);
 
+	@Override
+	public abstract int hashCode();
+	@Override
+	public abstract boolean equals(Object obj);
+
 }
